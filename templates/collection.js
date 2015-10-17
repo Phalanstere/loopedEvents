260068c85095a2ca2c88c981f588df931f3daa4c
Phalanstere <burckhardt@ludicmedia.de>
2015-10-17 15:20:24 +0200

    version 0.48, templates added

---
+++ templates/collection.js
@@ -0,0 +1,25 @@
+var e;
+
+e = {
+        type: "splittext",
+        subtype: "machine_gun",
+        div: "Fenster",
+        time: 5500,
+        duration: 7700,
+        rhythm: 300,
+        desc: "Hintergrund wird rot",  
+        css: { color: "white", scale: 3, ease: "Elastic.easeOut"}, 
+        };
+
+
+
+ e = {
+        type: "splittext",
+        subtype: "explosion",
+        splitting: "letters",
+        div: "Fenster",
+        time: 5500,
+        duration: 400,
+        desc: "Explosion",  
+        css: { color: "white", scale: 3, ease: "Elastic.easeOut"}, 
+        };
